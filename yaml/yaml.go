21c380128d1373bd00743b8aa39160927a55167b
Shane <innon.shen@gmail.com>
2023-03-22 16:16:42 +0800

    yaml 注释

--- yaml/yaml.go
+++ yaml/yaml.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 )
 
+// Load 将 yaml 文件中的数据加载到 model 中
+// 注意，这里需要传入的是 model 的地址，见测试用例 TestLoad
 func Load(path string, model interface{}) error {
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
