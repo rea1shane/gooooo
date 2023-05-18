d2447dc5c8df1d7b53c636b6f94a6761df92ac7f
Shane <innon.shen@gmail.com>
2023-05-18 14:11:31 +0800

    update error

--- error/error_test.go
+++ error/error_test.go
@@ -8,10 +8,66 @@ import (
 )
 
 func TestWrapError(t *testing.T) {
-	fmt.Printf("%+v", httpRequest())
+	fmt.Printf("%+v\n", a())
 }
 
-func httpRequest() error {
+func request() error {
 	_, err := http.Get("http://localhost:8888")
 	return failure.Wrap(err)
+}
+
+const (
+	notFound failure.StringCode = "NotFound"
+)
+
+func a() error {
+	return failure.Wrap(&aError{
+		msg: "A",
+		err: b(),
+	})
+}
+
+func b() error {
+	return failure.Wrap(&bError{
+		msg: "B",
+		err: c(),
+	})
+}
+
+func c() error {
+	return failure.New(notFound)
+}
+
+// aError 包装接收到的其他 error
+type aError struct {
+	msg string
+	err error
+}
+
+func (a *aError) Unwrap() error {
+	return a.err
+}
+
+func (a *aError) Error() string {
+	if a.err != nil {
+		return a.msg + " : " + a.err.Error()
+	}
+	return a.msg
+}
+
+// bError 包装接收到的其他 error
+type bError struct {
+	msg string
+	err error
+}
+
+func (b *bError) Unwrap() error {
+	return b.err
+}
+
+func (b *bError) Error() string {
+	if b.err != nil {
+		return b.msg + " : " + b.err.Error()
+	}
+	return b.msg
 }
