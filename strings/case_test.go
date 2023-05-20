ee92edde8582696f2ed3de587342beca8f89963a
Shane <innon.shen@gmail.com>
2023-05-20 16:47:48 +0800

    Update case_test.go

--- strings/case_test.go
+++ strings/case_test.go
@@ -5,6 +5,65 @@ import (
 	"testing"
 )
 
+var samples = []string{
+	"oneword",
+	"oneword123",
+	"twowords",
+	"twowords456",
+	"twowordsabc",
+	"twoabcwords",
+	"ONEWORD",
+	"ONEWORD123",
+	"TWOWORDS",
+	"TWOWORDS456",
+	"TWOWORDSABC",
+	"TWOABCWORDS",
+	"twoWords",
+	"twoWords456",
+	"twoWordsABC",
+	"twoABCWords",
+	"Oneword",
+	"Oneword123",
+	"TwoWords",
+	"TwoWords456",
+	"TwoWordsABC",
+	"TwoABCWords",
+	"two_words",
+	"two_words456",
+	"two_words_abc",
+	"two_abc_words",
+	"TWO_WORDS",
+	"TWO_WORDS456",
+	"TWO_WORDS_ABC",
+	"TWO_ABC_WORDS",
+	"Two_Words",
+	"Two_Words456",
+	"Two_Words_ABC",
+	"Two_ABC_Words",
+	"two-words",
+	"two-words456",
+	"two-words-abc",
+	"two-abc-words",
+	"TWO-WORDS",
+	"TWO-WORDS456",
+	"TWO-WORDS-ABC",
+	"TWO-ABC-WORDS",
+	"Two-Words",
+	"Two-Words456",
+	"Two-Words-ABC",
+	"Two-ABC-Words",
+}
+
 func TestCaseOf(t *testing.T) {
-	fmt.Println(CaseOf("ABC"))
+	result := make(map[Case][]string)
+	for _, sample := range samples {
+		result[CaseOf(sample)] = append(result[CaseOf(sample)], sample)
+	}
+	for _, c := range allCases {
+		fmt.Println(c)
+		fmt.Println(result[c])
+		fmt.Println()
+	}
+	fmt.Println(Unknown)
+	fmt.Println(result[Unknown])
 }
